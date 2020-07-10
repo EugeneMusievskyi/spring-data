12f28c8b7d6a0bba6e9f5284388912b08eb36572
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-11 01:36:00 +0300

    Role query created

--- src/main/java/com/bsa/springdata/role/RoleRepository.java
+++ src/main/java/com/bsa/springdata/role/RoleRepository.java
@@ -1,8 +1,16 @@
 package com.bsa.springdata.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Role r WHERE r.code = :code AND r.users.size = 0")
+    void deleteByCode(@Param("code") String code);
 }
