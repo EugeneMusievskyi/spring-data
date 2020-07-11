32a5f4c77f2a1518860dd1f2562a7923b821d113
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-11 06:11:11 +0300

    office queries created

--- src/main/java/com/bsa/springdata/office/OfficeRepository.java
+++ src/main/java/com/bsa/springdata/office/OfficeRepository.java
@@ -1,8 +1,30 @@
 package com.bsa.springdata.office;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OfficeRepository extends JpaRepository<Office, UUID> {
+    @Query("SELECT distinct(o) FROM Office o " +
+            "INNER JOIN o.users u " +
+            "INNER JOIN u.team t " +
+            "INNER JOIN t.technology tech " +
+            "WHERE tech.name = :technology")
+    List<Office> findByTechnology(@Param("technology") String technology);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Office o SET o.address = :newAddress WHERE o.address = :oldAddress " +
+            "AND exists(SELECT u FROM o.users u WHERE u.team.project IS NOT NULL)")
+    void updateAddress(@Param("oldAddress") String oldAddress,
+                                   @Param("newAddress") String newAddress);
+
+    Optional<Office> findByAddress(String city);
+
 }
